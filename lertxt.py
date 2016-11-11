a2b66978c279fcb6bdb45d6d6dba1b44ff8a7e44
Hamilton Lopes <hamilton_dino02@hotmail.com>
2016-11-11 00:32:17 -0300

    alteração

--- lertxt.py
+++ lertxt.py
@@ -25,12 +25,17 @@ if __name__ == '__main__':
     arqR = open('resposta.txt', 'w')
     texto = []
     var = 0
+    texto.append('ACME Inc.                 Uso do espaço em disco pelos usuarios\n')
+    texto.append('---------------------------------------------------------------\n')
+    texto.append('Nr.\tUsuário\tEspaço Utilizado\t% do uso\n\n')
     for i in range(int((len(lista))/2)):
         texto.append('%d\t' %(i))
         texto.append(lista[var]+'\t')
         texto.append('%0.2f%s\t' %(lista[var+1],' MB') )
         texto.append('%0.2f%s\n' %(arredondar(lista[var+1]*100)/sum(lista[1::2]),'%'))
         var+=2
+    texto.append('\n%s %0.2f %s\n' %('Espaço total ocupado:', sum(lista[1::2]), 'MB'))
+    texto.append('%s %0.2f %s\n' %('Espaço medio ocupado:', sum(lista[1::2])/len(lista[1::2]), 'MB'))
     arqR.writelines(texto)
     arqR.close()
 
