b90f711695b8f18439c2c4fa974384dc1374a705
Hamilton Lopes <hamilton_dino02@hotmail.com>
2016-11-11 01:09:47 -0300

    alteração final

--- lertxt.py
+++ lertxt.py
@@ -9,31 +9,32 @@ if __name__ == '__main__':
     texto = arq.readlines()
     lista = []
     for linha in texto:
-        name = ""
-        byte = ""
+        value, byte = "", ""
         for a in linha:
             if a.isdigit():
                 byte += a
             else:
                 if not(a == ' ' or ord(a) == 10):
-                    name += a
-        lista.append(name)
+                    value += a
+        lista.append(value)
         lista.append(byte)
     arq.close()
 
     lista[1::2] = [arredondar(transformarByteEmMegaByte(x)) for x in lista[1::2]]
     arqR = open('resposta.txt', 'w')
     texto = []
-    var = 0
     texto.append('ACME Inc.                 Uso do espaço em disco pelos usuarios\n')
     texto.append('---------------------------------------------------------------\n')
     texto.append('Nr.\tUsuário\tEspaço Utilizado\t% do uso\n\n')
-    for i in range(int((len(lista))/2)):
-        texto.append('%d\t' %(i))
-        texto.append(lista[var]+'\t')
-        texto.append('%0.2f%s\t' %(lista[var+1],' MB') )
-        texto.append('%0.2f%s\n' %(arredondar(lista[var+1]*100)/sum(lista[1::2]),'%'))
-        var+=2
+
+    for i, value in enumerate(lista):
+        if i%2 == 0:
+            texto.append('%d\t' % (int(i/2)+1))
+            texto.append(value + '\t')
+        else:
+            texto.append('%0.2f%s\t' % (value, ' MB'))
+            texto.append('%0.2f%s\n' % ((value * 100) / sum(lista[1::2]), '%'))
+
     texto.append('\n%s %0.2f %s\n' %('Espaço total ocupado:', sum(lista[1::2]), 'MB'))
     texto.append('%s %0.2f %s\n' %('Espaço medio ocupado:', sum(lista[1::2])/len(lista[1::2]), 'MB'))
     arqR.writelines(texto)
